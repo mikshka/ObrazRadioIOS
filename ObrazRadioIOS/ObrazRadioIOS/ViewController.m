599e224eba46b2427aaf988660573316db619058
mikshka <michael.korshunov@gmail.com>
2016-09-03 15:03:55 +0300

    add listener to hardware volume button

--- ObrazRadioIOS/ObrazRadioIOS/ViewController.m
+++ ObrazRadioIOS/ObrazRadioIOS/ViewController.m
@@ -25,6 +25,22 @@
     [super viewDidAppear:animated];
     [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
     [self becomeFirstResponder];
+    
+    AVAudioSession* audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:YES error:nil];
+    [audioSession addObserver:self
+                   forKeyPath:@"outputVolume"
+                      options:0
+                      context:nil];
+    
+}
+
+-(void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    
+    if ([keyPath isEqual:@"outputVolume"]) {
+        float volumeLevel = [[MPMusicPlayerController applicationMusicPlayer] volume];
+        [self.slider setValue:volumeLevel animated:YES];
+    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated
